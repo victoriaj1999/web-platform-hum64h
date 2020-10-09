8977a29d3f0df6b059eaee25a8b17d05fcfe54ee
victoriaj1999 <victoriajohnston33@gmail.com>
2020-10-09 12:19:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-scywbv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-scywbv)
---
+++ script.js
@@ -0,0 +1,21 @@
+var username = new Array();
+username1 = "VictoriaJ";
+username2 = "NathanS";
+username3 = "Wayne01";
+username4 = "Lewis2";
+var password = new Array();
+password1 = "password01";
+password2 = "password02";
+password3 = "password03";
+password4 = "password04";
+
+if (username == username){
+  if (password == password){
+    alert("Login successfull");
+    location= "gallery.html";
+  }
+}else{
+  alert("Username or/ and password is incorrect");
+}
+
+//http://www.dynamicdrive.com/forums/showthread.php?73247-Help-with-a-Password-Box-to-redirect-users-to-another-page//
